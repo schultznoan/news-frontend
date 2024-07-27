a0de58a0dc650dea7bfe057722fb5b246ba0b86e
schultznoan <danchoo14@bk.ru>
2024-07-27 11:33:15 +0300

    feat: init fsd arch

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from './app.vue'
+import router from './router/index'
 
 const app = createApp(App)
 
